6a462c00b1a1fb2fa0ec456e20ebecf9f6825894
Sangam Pardhi <sangampardhi2004@gmail.com>
2026-03-25 13:48:26 +0530

    Created a server

---
+++ homework.md
@@ -0,0 +1,10 @@
+- Create a repository
+- Initialize the repository
+- node_modules, package.json, package-lock.json
+- Install express
+- Create a server
+- Listen to port 7777
+- Write request handlers for /test, /hello
+- Install nodemon and update scripts inside package.json
+- What is the use of "-g" while npm install 
+- Difference between caret and tilda
---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/",(req, res) =>{
+    res.send('Namaste from the dashboard!');
+});
+
+app.use("/hello",(req, res) =>{
+    res.send('Hello hello hello!');
+});
+
+app.use("/test",(req, res) =>{
+    res.send('Hello from the server!');
+});
+
+app.listen(7777, ()=>{
+    console.log('server is successfully connected to port 7777..')
+});
+
